b95add7c67ac174939a506d7ad6374770900e29a
Arne Beer <contact@arne.beer>
2023-02-14 23:29:16 +0100

    chore: Bump dependencies

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,10 @@
 use clap::{ArgAction, Parser};
 
 #[derive(Parser, Debug)]
-#[clap(name = "Sniff", about = "Track wifi devices", author, version)]
+#[command(name = "Sniff", about = "Track wifi devices", author, version)]
 pub struct CliArguments {
     /// Verbose mode (-v, -vv, -vvv)
-    #[clap(short, long, action = ArgAction::Count)]
+    #[arg(short, long, action = ArgAction::Count)]
     pub verbose: u8,
 
     /// The device you want to listen on (e.g. [wlan0, wlp3s0])
